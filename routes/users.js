85c0901aa85a3c8eb2ed0f1598361e75933e7d61
aprajita <aprajita.aap67@gmail.com>
2020-11-17 19:22:41 +0530

    Installler.js

--- routes/users.js
+++ routes/users.js
@@ -1,32 +1,50 @@
 var express = require('express');
 var router = express.Router();
+const Users = require('./Controler/Users');
+const cors = require('cors');
 
 /* GET users listing. */
-router.get('/', (req, res) => {
-  res.render('url_page');
- });
+router.get('/',Users.Urlpage);
  
- // Index page views
- router.get('/index', (req, res) => {
-  const shop = req.query.shop;
-  res.render('index',{shop:shop});
-});
+//  // Index page views
+//  router.get('/index',Users.Redirectpage);
 
-// // script router
-// router.get('/server', (req, res) => {
-//   res.sendFile(__dirname+'/script/load.js');
-//  });
- router.get('/style', (req, res) => {
-   res.render('main');
- });
- router.get('/document', (req, res) => {
- // res.render('main');
- res.sendFile(__dirname+'/script/document/Smart-Announcement-Bar-Document.pdf');
-});
-router.get('/load/css', (req, res) => {
-  // res.render('main');
-  res.sendFile(__dirname+'/script/document/load.css');
- });
+  // // Index page views
+  // router.get('/document',Users.Redirectpage);
+
+  router.get('/index', (req, res) => {
+    const shop = req.query.shop;
+    const index = "/index?shop="+shop;
+    const document = "/document?shop="+shop; 
+
+  
+    res.render('index',{
+      shop:shop,
+      index:index,
+      document:document
+
+    });
+  });
+
+  router.get('/document',(req, res)=> {
+
+    res.sendFile(__dirname+'/Controler/script/document/Smart Mobile aap banner document.pdf'); 
+  
+    // res.render('document',{
+    //   shop:shop,
+    //   index:index,
+    //   document:document
+    //  });
+  });
+
+// script router
+router.get('/server',Users.Scripttag);
+
+router.get("/path",cors(),(req,res)=>{
+    res.send("heyyyy");
+})
+
+module.exports = router;
 
 
 
